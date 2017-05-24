a9f09fcc81513ca8aefffadbf1289b4637763fca
igoldyrev <goldirev12@gmail.com>
2017-05-24 16:37:45 +0500

    Создан первый тестовый макет, прикреплена шапка и теги к макету

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php include ($_SERVER["DOCUMENT_ROOT"]."/frames/headtags.html");?>
+
+</head><body>
+<div class="container">
+  <div class="header">
+	<?php include ($_SERVER["DOCUMENT_ROOT"]."/frames/header.html");?>
+  </div>
+  
+  <div class="main">
+    Основной блок
+  </div>
+  
+  <div class="footer">
+    Это подвал
+  </div>
+</div>
