c39b7b6854809ac8c44fc0b7fa2f5c8bbdb9cf80
Ilya Goldyrev <goldyrev.i.prm@uremont.com>
2017-11-24 11:46:52 +0500

    Установлен вебпак и зависимости

---
+++ webpack.config.js
@@ -0,0 +1,120 @@
+var webpack = require('webpack');
+var path = require('path');
+var glob = require('glob');
+var glob = require('glob-all');
+
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var PurifyCSSPlugin = require('purifycss-webpack');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+
+var InProduction = (process.env.NODE_ENV === 'production');
+
+module.exports = {
+    entry: {
+
+        gaross: [
+                './js/main.js',
+                './modules/style.scss'
+
+        ],
+        //vendor: ['jquery']
+
+    },
+
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: '[name].js'
+
+    },
+
+    module: {
+
+        rules: [
+
+            {
+                test: /\.s[ac]ss$/,
+                use: ExtractTextPlugin.extract({
+                    use: ['css-loader', 'sass-loader'],
+                    fallback: 'style-loader'
+
+                })
+
+            },
+
+            {
+                test: /\.css$/,
+                use: ['css-loader']
+            },
+
+            {
+
+                test: /\.(svg|eot|ttf|woff|woff2)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '/fonts/[name].[ext]'
+                }
+            },
+
+            {
+
+                test: /\.(png|jpe?g|gif)$/,
+                loaders: [
+                    {
+
+                        loader: 'file-loader',
+                        options: {
+                            name: 'images/[name].[ext]'
+
+                        }
+                    },
+
+                    'img-loader'
+
+                ]
+
+            },
+
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+        ]
+    },
+
+    plugins: [
+
+        new ExtractTextPlugin('[name].css'),
+
+        /*new PurifyCSSPlugin({
+            // Give paths to parse for rules. These should be absolute!
+            paths: glob.sync([
+
+                path.join(__dirname, '*.html'),
+                path.join(__dirname, '*.php'),
+
+        ]),
+            minimize: InProduction
+
+        }),*/
+
+        new CleanWebpackPlugin(['build'], {
+            root: __dirname,
+            verbose: true,
+            dry: false
+        }),
+
+        new webpack.LoaderOptionsPlugin({
+
+            minimize: InProduction
+        })
+
+    ]
+
+};
+
+if (InProduction) {
+    module.exports.plugins.push(
+        new webpack.optimize.UglifyJsPlugin()
+    );
+}
