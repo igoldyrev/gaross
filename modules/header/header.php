17a590bb1673a6b32ca55d0c79f6c6af6d361ee9
Ilya Goldyrev <goldyrev.i.prm@uremont.com>
2017-11-24 12:02:57 +0500

    Разметка для новой шапки

---
+++ modules/header/header.php
@@ -0,0 +1,10 @@
+<header class="header">
+    <img class="header__logo" src="/img/logo.png" alt="Гаросс Пермь">
+    <div class="header__info">
+        <ul class="header__list">
+            <li>г. Пермь, ул. Дзержинского 15</li>
+            <li><a href="tel:+73422371597" class="header__link">+7 (342) 237 15 97</a></li>
+            <li><a href="mailto:info@gaross59.ru" class="header__link">info@gaross59.ru</a></li>
+        </ul>
+    </div>
+</header>
