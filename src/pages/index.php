42ef8a2827e7deaf26275ec7840027d538474a6b
Ilya Goldyrev <goldirev12@gmail.com>
2018-05-06 22:27:45 +0500

    develop Первичный вариант главной страницы

---
+++ src/pages/index.php
@@ -0,0 +1,17 @@
+<?php
+include($_SERVER["DOCUMENT_ROOT"] . "/blocks/metatags.php");
+include($_SERVER["DOCUMENT_ROOT"] . "/blocks/header.html");
+include($_SERVER["DOCUMENT_ROOT"] . "/blocks/navigation.html"); ?>
+
+<div class="wrapper">
+  <?php include($_SERVER["DOCUMENT_ROOT"] . "/blocks/left-nav.html"); ?>
+  <div class="wrapper__content">
+    <h1 class="title title-h1">ООО «ГАРОСС» успешно работает с 2010 года</h1>
+    <p class="text">Основное направление деятельности предприятия - комплексные решения в области строительства,
+      проектирования, энергетики, обеспечения безопасности и связи государственных и коммерческих структур, предприятий
+      энергетического, нефтегазового и оборонного комплекса, а также объектов железнодорожного и воздушного
+      транспорта.</p>
+  </div>
+</div>
+<?php include($_SERVER["DOCUMENT_ROOT"] . "/blocks/footer.html");
+include($_SERVER["DOCUMENT_ROOT"] . "/blocks/counters.html"); ?>
