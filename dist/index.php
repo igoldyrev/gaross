51f2d5dc7a08522192b48c7f14030d482dc0a800
Ilya Goldyrev <goldirev12@gmail.com>
2018-05-15 22:45:22 +0500

    develop Верстка страницы по блокам

--- dist/index.php
+++ dist/index.php
@@ -4,11 +4,25 @@ include($_SERVER["DOCUMENT_ROOT"] . "/blocks/header.html");
 include($_SERVER["DOCUMENT_ROOT"] . "/blocks/navigation.html"); ?>
 
 <div class="wrapper">
+  <div class="tab tab__about tab--active">
     <h1 class="title title-h1">ООО «ГАРОСС» успешно работает с 2010 года</h1>
     <p class="text">Основное направление деятельности предприятия - комплексные решения в области строительства,
       проектирования, энергетики, обеспечения безопасности и связи государственных и коммерческих структур, предприятий
       энергетического, нефтегазового и оборонного комплекса, а также объектов железнодорожного и воздушного
       транспорта.</p>
+  </div>
+  <div class="tab tab__licenses">
+    Страница с лицензиями
+  </div>
+  <div class="tab tab__orders">
+    Заказчики
+  </div>
+  <div class="tab tab__letters">
+    Письма
+  </div>
+  <div class="tab tab__contacts">
+    Контакты
+  </div>
 </div>
 <?php include($_SERVER["DOCUMENT_ROOT"] . "/blocks/footer.html");
 include($_SERVER["DOCUMENT_ROOT"] . "/blocks/counters.html"); ?>
