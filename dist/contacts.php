cd95a1b9ce5993cdbc5a7e9f835f39a08bbc86b4
Ilya Goldyrev <goldirev12@gmail.com>
2018-05-18 22:11:31 +0500

    develop Добавлен контент на странице контактов

--- dist/contacts.php
+++ dist/contacts.php
@@ -4,5 +4,27 @@ include($_SERVER["DOCUMENT_ROOT"] . "/blocks/header.html");
 include($_SERVER["DOCUMENT_ROOT"] . "/blocks/navigation.html"); ?>
 
 <div class="wrapper">
-  Контакты
+  <h1 class="title title-h1">Компания Гаросс</h1>
+  <h2 class="title title-h2">Адреса:</h2>
+  <p class="text"><strong>Офис:</strong> г.Пермь, ул. Дзержинского 15</p>
+  <p class="text"><strong>Производственная база:</strong> г.Пермь, ул. Дзержинского 47</p>
+  <h2 class="title title-h2">Связаться с нами</h2>
+  <div class="clearfix">
+    <a class="link contacts__link link--color-hover" href="tel:+73422371597">Телефон/факс +7 (342) 237 15 97</a>
+    <a class="link contacts__link link--color-hover" href="mailto:info@gaross59.ru">Электронная почта: info@gaross59.ru</a>
+  </div>
+  <h2 class="title title-h2">Реквизиты:</h2>
+  <p class="text">ИНН 5903099470 КПП 590301001</p>
+  <p class="text">р/с: 40702810949490059143 в Волго-Вятском  банке ПАО  Сбербанк  г. Нижний Новгород; кор.счет: 30101810900000000603; БИК 042202603</p>
+  <p class="text">ОГРН 1105903000103</p>
+  <p class="text">Код по ОКПО 63329089</p>
+  <p class="text">ОКВЭД (основной вид деятельности) – 41.20</p>
+  <p class="text">Генеральный директор – Голдырев Андрей Николаевич, действующий на основании Устава</p>
+
+  <div class="">
+    Наше местоположение
+    //todo карта яндекс
+  </div>
+
+
 </div>
