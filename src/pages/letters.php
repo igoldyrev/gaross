9a37f23435a54271fb048fcb8e6a20190a7e5ad6
Ilya Goldyrev <goldirev12@gmail.com>
2018-05-22 23:25:40 +0500

    develop Изменена верстка и стили раздела с благодарственными письмами

--- src/pages/letters.php
+++ src/pages/letters.php
@@ -5,18 +5,17 @@ include($_SERVER["DOCUMENT_ROOT"] . "/blocks/navigation.html"); ?>
 
 <div class="wrapper">
   <h1 class="title title-h1">Благодарственные письма</h1>
-  <div class="letter__wrap">
-    <div class="letter">
-      <h3 class="title title-h3 letter__title">ФКГУ Комбинат "Алый Стяг"</h3>
-      <img class="img letter__img" src="/img/letters/alyi-styag-osveshenie.jpg" alt="">
-    </div>
-    <div class="letter">
-      <h3 class="title title-h3 letter__title">ФКГУ Комбинат "Алый Стяг"</h3>
-      <img class="img letter__img" src="/img/letters/alyi-styag-psd.jpg" alt="">
-    </div>
-    <div class="letter">
-      <h3 class="title title-h3 letter__title">ООО ЦБК Кама"</h3>
-      <img class="img letter__img" src="/img/letters/cbk-kama.jpg" alt="">
-    </div>
+  <div class="letter">
+    <h4 class="title title-h4">ФКГУ Комбинат "Алый Стяг"</h4>
+    <a class="letter__img" href="/img/letters/alyi-styag-osveshenie.jpg">Благодарственное письмо за своевременное и
+      оперативное выполнение работ по монтажу освещения и электросилового оборудования складов</a>
+    <a class="letter__img" href="/img/letters/alyi-styag-psd.jpg">Благодарственное письмо за разработку
+      проектно-сметной документации на капитальный ремонт сети водоснабжения, пожарного водовода с полуподземной
+      насосной камерой</a>
+  </div>
+  <div class="letter">
+    <h4 class="title title-h4">ООО ЦБК Кама"</h4>
+    <a class="letter__img" href="/img/letters/cbk-kama.jpg">Благодарственное письмо за выполненные
+      строительно-ремонтные работы на территории ООО "ЦБК КАМА"</a>
   </div>
 </div>
