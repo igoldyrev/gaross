b5c3db3115911cc7adf4a1e33d1284c698db72c8
Ilya Goldyrev <goldirev12@gmail.com>
2018-05-23 22:12:36 +0500

    develop Верстка страницы с лицензиями

--- dist/licenses.php
+++ dist/licenses.php
@@ -4,5 +4,30 @@ include($_SERVER["DOCUMENT_ROOT"] . "/blocks/header.html");
 include($_SERVER["DOCUMENT_ROOT"] . "/blocks/navigation.html"); ?>
 
 <div class="wrapper">
-  Лицензии
+  <h1 class="title title-h1">Лицензии</h1>
+  <div class="license" id="mchs">
+    <h2 class="title title-h2">Лицензия МЧС</h2>
+    <p class="text">На осуществление деятельности по монтажу , техническому обслуживанию и ремонту средств обеспечения
+      пожарной безопасности зданий и сооружений</p>
+    <div class="license__wrap">
+      <img class="license__img" src="/img/licenses/mchs-1.jpg" alt="">
+    </div>
+    <a class="link license__link" target="_blank" href="/img/licenses/mchs-pdf.pdf">Смотреть в PDF</a>
+  </div>
+  <div class="license" id="sro-stroiteli-urala">
+    <h2 class="title title-h2">СРО "Строители Урала"</h2>
+    <p class="text">Свидетельство о допуске к определенным видам работ, которые оказывают влияние на безопасность
+      объектов капитального строительства</p>
+    <div class="license__wrap">
+      <img class="license__img" src="/img/licenses/smr-220317-0.jpg" alt="">
+    </div>
+    <a class="link license__link" target="_blank" href="/img/licenses/smr-220317-pdf.pdf">Смотреть в PDF</a>
+  </div>
+  <div class="license" id="sro-project-work">
+    <h2 class="title title-h2">СРО на проектные работы</h2>
+    <div class="license__wrap">
+      <img class="license__img" src="/img/licenses/project-work-0.jpg" alt="">
+    </div>
+    <a class="link license__link" target="_blank" href="/img/licenses/project-work-pdf.pdf">Смотреть в PDF</a>
+  </div>
 </div>
