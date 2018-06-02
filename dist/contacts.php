22e3858fee58268ecfd67e99740de8b5eefdcb08
Ilya Goldyrev <goldirev12@gmail.com>
2018-06-02 21:47:44 +0500

    develop Правки и добавление карты

--- dist/contacts.php
+++ dist/contacts.php
@@ -4,27 +4,23 @@ include($_SERVER["DOCUMENT_ROOT"] . "/blocks/header.html");
 include($_SERVER["DOCUMENT_ROOT"] . "/blocks/navigation.html"); ?>
 
 <div class="wrapper">
-  <h1 class="title title-h1">Компания Гаросс</h1>
-  <h2 class="title title-h2">Адреса:</h2>
-  <p class="text"><strong>Офис:</strong> г.Пермь, ул. Дзержинского 15</p>
-  <p class="text"><strong>Производственная база:</strong> г.Пермь, ул. Дзержинского 47</p>
+  <h1 class="title title-h1">Общество с ограниченной ответсвенностью "Гаросс"</h1>
+  <p class="text"><strong>Офис:</strong> г.Пермь, ул. Дзержинского 15, офис 10</p>
+  <p class="text"><strong>Производственная база:</strong> г.Пермь, ул. Дзержинского 47 Литера Б1</p>
   <h2 class="title title-h2">Связаться с нами</h2>
   <div class="clearfix">
     <a class="link contacts__link link--color-hover" href="tel:+73422371597">Телефон/факс +7 (342) 237 15 97</a>
-    <a class="link contacts__link link--color-hover" href="mailto:info@gaross59.ru">Электронная почта: info@gaross59.ru</a>
+    <a class="link contacts__link link--color-hover" href="mailto:info@gaross59.ru">Электронная почта:
+      info@gaross59.ru</a>
   </div>
+  <h2 class="title title-h2">Наше местоположение</h2>
+  <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Afdac42df5209134c889acf92bd590f2a3abff6b62f9a27c25964c1482b995b23&amp;width=100%25&amp;height=450&amp;lang=ru_RU&amp;scroll=true"></script>
   <h2 class="title title-h2">Реквизиты:</h2>
   <p class="text">ИНН 5903099470 КПП 590301001</p>
-  <p class="text">р/с: 40702810949490059143 в Волго-Вятском  банке ПАО  Сбербанк  г. Нижний Новгород; кор.счет: 30101810900000000603; БИК 042202603</p>
+  <p class="text">р/с: 40702810949490059143 в Волго-Вятском банке ПАО Сбербанк г. Нижний Новгород; кор.счет:
+    30101810900000000603; БИК 042202603</p>
   <p class="text">ОГРН 1105903000103</p>
   <p class="text">Код по ОКПО 63329089</p>
   <p class="text">ОКВЭД (основной вид деятельности) – 41.20</p>
   <p class="text">Генеральный директор – Голдырев Андрей Николаевич, действующий на основании Устава</p>
-
-  <div class="">
-    Наше местоположение
-    //todo карта яндекс
-  </div>
-
-
 </div>
