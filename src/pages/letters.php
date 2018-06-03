9587553b0fc1806d02195f38f8c82a968f35caae
Ilya Goldyrev <goldirev12@gmail.com>
2018-06-03 21:34:37 +0500

    develop Доработка страницы с благодарственными письмами и добавление на нее иконок fa

--- src/pages/letters.php
+++ src/pages/letters.php
@@ -5,29 +5,30 @@ include($_SERVER["DOCUMENT_ROOT"] . "/blocks/navigation.html"); ?>
 
 <div class="wrapper">
   <h1 class="title title-h1">Благодарственные письма</h1>
-  <div class="letter">
+  <div class="letter clearfix">
     <h4 class="title title-h4">ФКГУ Комбинат "Алый Стяг"</h4>
-    <a class="letter__img" href="/img/letters/alyi-styag-osveshenie.jpg">Благодарственное письмо за своевременное и
-      оперативное выполнение работ по монтажу освещения и электросилового оборудования складов</a>
-    <a class="letter__img" href="/img/letters/alyi-styag-psd.jpg">Благодарственное письмо за разработку
-      проектно-сметной документации на капитальный ремонт сети водоснабжения, пожарного водовода с полуподземной
-      насосной камерой</a>
+    <a class="letter__img" href="/img/letters/alyi-styag-osveshenie.jpg"><i class="fa fa-file-text-o letter__icon"></i>Монтаж
+      освещения и электросилового оборудования складов</a>
+    <a class="letter__img" href="/img/letters/alyi-styag-psd.jpg"><i class="fa fa-file-text-o letter__icon"></i>Разработка
+      проектно-сметной документации на капитальный ремонт сети водоснабжения</a>
   </div>
-  <div class="letter">
+  <div class="letter clearfix">
     <h4 class="title title-h4">НТЦ Информационная безопасность</h4>
-    <a class="letter__img" href="/img/letters/ntc-inf-bezopasnost-smr.jpg">Благодарственное письмо за организацию и
+    <a class="letter__img" href="/img/letters/ntc-inf-bezopasnost-smr.jpg"><i
+        class="fa fa-file-text-o letter__icon"></i>Организация и
       выполнение строительно-монтажных работ по капитальному ремонту освещения</a>
-    <a class="letter__img" href="/img/letters/ntc-inf-bezopasnost-pir.jpg">Благодарственное письмо за разработку
+    <a class="letter__img" href="/img/letters/ntc-inf-bezopasnost-pir.jpg"><i
+        class="fa fa-file-text-o letter__icon"></i>Разработка
       проектно-сметной документации по договору подряда №59 от 21 августа 2017г</a>
   </div>
-  <a class="letter__img" href="/img/letters/cbk-kama.jpg">Благодарственное письмо за выполненные
-    строительно-ремонтные работы на территории ООО "ЦБК КАМА"</a>
-  <a class="letter__img" href="/img/letters/ip-panfilov.jpg">Благодарственное письмо за выполненные работы по монтажу
-    системы видеонаблюдения на объекте ТЦ Апельсин</a>
-  <a class="letter__img" href="/img/letters/kontur.jpg">Благодарственное письмо за монтаж систем электроснабжения,
-    охранно-пожарной сигнализации, видеонаблюдения, связи</a>
-  <a class="letter__img" href="/img/letters/lpp-almed.jpg">Благодарственное письмо от санаторий-профилакторий Алмед</a>
-  <a class="letter__img" href="/img/letters/metafraks.jpg">Благодарственное письмо от ОАО Метафракс</a>
+  <div class="letter__list clearfix">
+    <a class="letter__img" href="/img/letters/cbk-kama.jpg"><i class="fa fa-file-text-o letter__icon"></i>ООО "ЦБК КАМА"</a>
+    <a class="letter__img" href="/img/letters/ip-panfilov.jpg"><i class="fa fa-file-text-o letter__icon"></i>ТЦ Апельсин</a>
+    <a class="letter__img" href="/img/letters/kontur.jpg"><i class="fa fa-file-text-o letter__icon"></i>ООО "Контур"</a>
+    <a class="letter__img" href="/img/letters/lpp-almed.jpg"><i class="fa fa-file-text-o letter__icon"></i>Санаторий-профилакторий
+      Алмед</a>
+    <a class="letter__img" href="/img/letters/metafraks.jpg"><i class="fa fa-file-text-o letter__icon"></i>ОАО Метафракс</a>
+  </div>
 </div>
 <?php include($_SERVER["DOCUMENT_ROOT"] . "/blocks/footer.html");
 include($_SERVER["DOCUMENT_ROOT"] . "/blocks/counters.html"); ?>
