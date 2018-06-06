d001cb57c29906a5112d8344e0265c41cbb0c330
Ilya Goldyrev <goldirev12@gmail.com>
2018-06-06 23:15:48 +0500

    develop Примерная верстка и стили страницы с заказчиками

--- src/pages/orders.php
+++ src/pages/orders.php
@@ -4,7 +4,125 @@ include($_SERVER["DOCUMENT_ROOT"] . "/blocks/header.html");
 include($_SERVER["DOCUMENT_ROOT"] . "/blocks/navigation.html"); ?>
 
 <div class="wrapper">
-  Заказчики
+  <h1 class="title title-h1">Заказчики</h1>
+  <div class="order__wrap">
+    <div class="order">
+      <img class="order__logo" src="/img/orders/chmz-logo.png" alt="ЧМЗ">
+      <div class="order__description">
+        <p class="order__name">ЧМЗ</p>
+        <p class="order__annotation">Чепецкий механический завод - предприятие госкорпорации "Росатом".</p>
+      </div>
+    </div>
+    <div class="order">
+      <img class="order__logo" src="/img/orders/ppz-logo.png" alt="Пермский пороховой завод">
+      <div class="order__description">
+        <p class="order__name">ФКП "Пермский пороховой завод"</p>
+        <p class="order__annotation">Одно из крупнейших российских предприятий оборонного комплекса.</p>
+      </div>
+    </div>
+    <div class="order">
+      <img class="order__logo" src="/img/orders/protonpm-logo.png" alt="Протон ПМ">
+      <div class="order__description">
+        <p class="order__name">ПРОТОН ПМ</p>
+        <p class="order__annotation">Публичное акционерное общество «Протон — Пермские моторы» является одним из ведущих
+          предприятий российской космической отрасли.</p>
+      </div>
+    </div>
+    <div class="order">
+      <img class="order__logo" src="/img/orders/goznak-logo.png" alt="ГОЗНАК">
+      <div class="order__description">
+        <p class="order__name">ГОЗНАК</p>
+        <p class="order__annotation">Одно из самых современных полиграфических предприятий в Европе.</p>
+      </div>
+    </div>
+    <div class="order">
+      <img class="order__logo" src="/img/orders/cbkkama.png" alt="ЦБК Кама">
+      <div class="order__description">
+        <p class="order__name">ЦБК Кама</p>
+        <p class="order__annotation">Экслюзивный производитель мелованной бумаги в России!</p>
+      </div>
+    </div>
+    <div class="order">
+      <img class="order__logo" src="/img/orders/galo.png" alt="АО ГалоПолимер-Пермь">
+      <div class="order__description">
+        <p class="order__name">АО "ГалоПолимер-Пермь"</p>
+        <p class="order__annotation">Одно из крупнейших химических предприятий России.</p>
+      </div>
+    </div>
+    <div class="order">
+      <img class="order__logo" src="/img/orders/gaz.png" alt="ООО Газпром межрегионгаз Пермь">
+      <div class="order__description">
+        <p class="order__name">ООО "Газпром межрегионгаз Пермь"</p>
+        <p class="order__annotation">Компания, входящая в структуру ООО «Газпром межрегионгаз» (100% дочернее
+          предприятие ОАО «Газпром»). Осуществляет поставку природного газа всем категориям потребителей Пермского
+          края.</p>
+      </div>
+    </div>
+    <div class="order">
+      <img class="order__logo" src="/img/orders/iskra.gif" alt="ПАО НПО Искра">
+      <div class="order__description">
+        <p class="order__name">ПАО "НПО Искра"</p>
+        <p class="order__annotation">ПАО «Искра» работает для оборонной, нефтегазовой, машиностроительной,
+          сельскохозяйственной и других отраслей промышленности.</p>
+      </div>
+    </div>
+    <div class="order">
+      <img class="order__logo" src="/img/orders/krym.png" alt="ГУ МЧС России по республике Крым">
+      <div class="order__description">
+        <p class="order__name">ГУ МЧС России по республике Крым</p>
+        <p class="order__annotation"></p>
+      </div>
+    </div>
+    <div class="order">
+      <img class="order__logo" src="/img/orders/lukoil-logo.svg" alt="ЛУКОЙЛ">
+      <div class="order__description">
+        <p class="order__name">ЛУКОЙЛ</p>
+        <p class="order__annotation">Одно из крупнейших предприятий сегмента «геологоразведка, добыча нефти и газа».</p>
+      </div>
+    </div>
+    <div class="order">
+      <img class="order__logo" src="/img/orders/metafrax-logo.png" alt="МЕТАФРАКС">
+      <div class="order__description">
+        <p class="order__name">МЕТАФРАКС</p>
+        <p class="order__annotation">Одно из самых динамично развивающихся химических предприятий в современной России,
+          крупнейший производитель метанола и его производных.</p>
+      </div>
+    </div>
+    <div class="order">
+      <img class="order__logo" src="/img/orders/niipm.png" alt="АО НИИПМ">
+      <div class="order__description">
+        <p class="order__name">АО "НИИПМ"</p>
+        <p class="order__annotation">Одно из крупнейших предприятий оборонно-промышленного комплекса России,
+          занимающихся разработкой перспективных образцов порохов, твёрдых ракетных топлив, малогазовых и
+          воспламеняющихся составов.</p>
+      </div>
+    </div>
+    <div class="order">
+      <img class="order__logo" src="/img/orders/nikiret.png"
+           alt="НАУЧНО-ИССЛЕДОВАТЕЛЬСКИЙ И КОНСТРУКТОРСКИЙ ИНСТИТУТ РАДИОЭЛЕКТРОННОЙ ТЕХНИКИ">
+      <div class="order__description">
+        <p class="order__name">НАУЧНО-ИССЛЕДОВАТЕЛЬСКИЙ И КОНСТРУКТОРСКИЙ ИНСТИТУТ РАДИОЭЛЕКТРОННОЙ ТЕХНИКИ</p>
+        <p class="order__annotation">Современный научно-производственный комплекс.</p>
+      </div>
+    </div>
+    <div class="order">
+      <img class="order__logo" src="/img/orders/ros.jpg" alt="ОАО Ростелеком">
+      <div class="order__description">
+        <p class="order__name">ОАО "Ростелеком"</p>
+        <p class="order__annotation">Одна из крупнейших в России и Европе телекоммуникационных компаний национального
+          масштаба, присутствующая во всех сегментах рынка услуг связи и охватывающая миллионы домохозяйств в
+          России.</p>
+      </div>
+    </div>
+    <div class="order">
+      <img class="order__logo" src="/img/orders/ural.png" alt="ОАО МРСК Урала">
+      <div class="order__description">
+        <p class="order__name">ОАО "МРСК Урала"</p>
+        <p class="order__annotation">Компания, осуществляющая транспорт электроэнергии и присоединение потребителей к
+          электросетям на территории Свердловской, Челябинской областей, а также Пермского края.</p>
+      </div>
+    </div>
+  </div>
 </div>
 <?php include($_SERVER["DOCUMENT_ROOT"] . "/blocks/footer.html");
 include($_SERVER["DOCUMENT_ROOT"] . "/blocks/counters.html"); ?>
