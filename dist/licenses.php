7ccd8267b3ddd64e4d203177eec2bc322eb58733
Ilya Goldyrev <goldirev12@gmail.com>
2018-06-09 20:42:46 +0500

    develop Правки верстки и стилей страницы с лицензиями

--- dist/licenses.php
+++ dist/licenses.php
@@ -10,25 +10,31 @@ include($_SERVER["DOCUMENT_ROOT"] . "/blocks/navigation.html"); ?>
     <p class="text">На осуществление деятельности по монтажу , техническому обслуживанию и ремонту средств обеспечения
       пожарной безопасности зданий и сооружений</p>
     <div class="license__wrap">
-      <img class="license__img" src="/img/licenses/mchs-1.jpg" alt="">
+      <img class="license__img" src="/img/licenses/mchs-1.jpg" alt="Лицензия МЧС<">
     </div>
-    <a class="link license__link" target="_blank" href="/img/licenses/mchs-pdf.pdf">Смотреть в PDF</a>
+    <div class="license__link-inner">
+      <i class="fa fa-file-pdf-o license__icon"></i><a class="link license__link" target="_blank" href="/img/licenses/mchs-pdf.pdf">Смотреть в PDF</a>
+    </div>
   </div>
   <div class="license" id="sro-stroiteli-urala">
     <h2 class="title title-h2">СРО "Строители Урала"</h2>
     <p class="text">Свидетельство о допуске к определенным видам работ, которые оказывают влияние на безопасность
       объектов капитального строительства</p>
     <div class="license__wrap">
-      <img class="license__img" src="/img/licenses/smr-220317-0.jpg" alt="">
+      <img class="license__img" src="/img/licenses/smr-220317-0.jpg" alt="СРО Строители Урала">
     </div>
-    <a class="link license__link" target="_blank" href="/img/licenses/smr-220317-pdf.pdf">Смотреть в PDF</a>
+    <div class="license__link-inner">
+      <i class="fa fa-file-pdf-o license__icon"></i><a class="link license__link" target="_blank" href="/img/licenses/smr-220317-pdf.pdf">Смотреть в PDF</a>
+    </div>
   </div>
   <div class="license" id="sro-project-work">
     <h2 class="title title-h2">СРО на проектные работы</h2>
     <div class="license__wrap">
-      <img class="license__img" src="/img/licenses/project-work-0.jpg" alt="">
+      <img class="license__img" src="/img/licenses/project-work-0.jpg" alt="СРО на проектные работы">
     </div>
-    <a class="link license__link" target="_blank" href="/img/licenses/project-work-pdf.pdf">Смотреть в PDF</a>
+    <div class="license__link-inner">
+      <i class="fa fa-file-pdf-o license__icon"></i><a class="link license__link" target="_blank" href="/img/licenses/project-work-pdf.pdf">Смотреть в PDF</a>
+    </div>
   </div>
 </div>
 <?php include($_SERVER["DOCUMENT_ROOT"] . "/blocks/footer.html");
