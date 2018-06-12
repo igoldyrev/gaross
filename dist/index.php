45cc160de12c13db3ea513401b02fa49c5107abc
Ilya Goldyrev <goldirev12@gmail.com>
2018-06-12 19:22:49 +0500

    develop Примерная верстка главной страницы

--- dist/index.php
+++ dist/index.php
@@ -5,29 +5,40 @@ include($_SERVER["DOCUMENT_ROOT"] . "/blocks/header.html");
 include($_SERVER["DOCUMENT_ROOT"] . "/blocks/navigation.html"); ?>
 
 <div class="wrapper">
-  <h1 class="title title-h1">ООО «ГАРОСС» успешно работает с 2010 года</h1>
+  <h1 class="title title-h1 index__title">ООО «ГАРОСС» успешно работает с 2010 года</h1>
   <p class="text">Основное направление деятельности предприятия - комплексные решения в области строительства,
     проектирования, энергетики, обеспечения безопасности и связи государственных и коммерческих структур, предприятий
     энергетического, нефтегазового и оборонного комплекса, а также объектов железнодорожного и воздушного
     транспорта.</p>
-  <p class="text">ООО «ГАРОСС» выполняет полный цикл работ по:</p>
-  <ul class="list list__unsorted">
-    <li>комплексному промышленному и гражданскому строительству;</li>
-    <li>проектированию;</li>
-    <li>поставке, монтажу и пуско-наладке всего необходимого оборудования для систем водоснабжения и канализации,
-      вентиляции, бесперебойного электропитания, сигнализации и связи;
-    </li>
-    <li>гарантийному и постгарантийному техническому обслуживанию.</li>
-  </ul>
-  <p class="text">Для решения данных задач ООО «ГАРОСС» имеет все необходимые лицензии, <a
-      class="link link--color-hover"
-      href="licenses.php/#sro-stroiteli-urala">свидетельство СРО «Строители Урала» № С – 171-59-0961-59-220317 от
-      22.03.2017 г.</a>, <a class="link link--color-hover" href="licenses.php/#sro-project-work">свидетельство СРО
-      «Проектные организации Урала» № П –
-      112-59-0170-59-290317 от 29.03.2017 г.</a>, <a class="link link--color-hover" href="licenses.php/#mchs">лицензия
-      МЧС № 59-Б/00513 от 13.07.2017
-      г.</a>, сертификат соответствия ISO, а также располагает высококвалифицированным коллективом проектировщиков,
-    менеджеров, инженеров, монтажников, прошедшим обучение и сертификацию.</p>
+  <h2 class="title title-h2">Основные виды работ:</h2>
+  <div class="index__work-wrap">
+    <div class="index__work">
+      <img class="index__img" src="http://via.placeholder.com/350x150" alt="">
+      <h2 class="index__name">Комплексное промышленное и гражданское строительство</h2>
+    </div>
+    <div class="index__work">
+      <img class="index__img" src="http://via.placeholder.com/350x150" alt="">
+      <h2 class="index__name">Проектирование</h2>
+    </div>
+    <div class="index__work">
+      <img class="index__img" src="http://via.placeholder.com/350x150" alt="">
+      <h2 class="index__name">Поставка, монтаж и пуско-наладка всего необходимого оборудования для систем водоснабжения
+        и канализации, вентиляции, бесперебойного электропитания, сигнализации и связи</h2>
+    </div>
+    <div class="index__work">
+      <img class="index__img" src="http://via.placeholder.com/350x150" alt="">
+      <h2 class="index__name">Гарантийное и постгарантийное техническое обслуживание</h2>
+    </div>
+  </div>
+  <h2 class="title title-h2">Для решения данных задач ООО «ГАРОСС» имеет все необходимые лицензии</h2>
+  <p class="text">Свидетельство <a class="link" href="licenses.php/#sro-stroiteli-urala">СРО «Строители Урала»</a> № С –
+    171-59-0961-59-220317 от 22.03.2017 г.</p>
+  <p class="text">Свидетельство <a class="link" href="licenses.php/#sro-project-work">СРО «Проектные организации Урала»</a> № П –
+    112-59-0170-59-290317 от 29.03.2017 г.</p>
+  <p class="text">Лицензия <a class="link" href="licenses.php/#mchs">МЧС № 59-Б/00513</a> от 13.07.2017 г.</p>
+  <p class="text">Сертификат соответствия ISO</p>
+  <p class="text">А также располагает высококвалифицированным коллективом проектировщиков, менеджеров, инженеров,
+    монтажников, прошедшим обучение и сертификацию.</p>
 </div>
 <?php include($_SERVER["DOCUMENT_ROOT"] . "/blocks/footer.html");
 include($_SERVER["DOCUMENT_ROOT"] . "/blocks/counters.html"); ?>
