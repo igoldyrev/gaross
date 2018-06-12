a6927097afa1941408075c700e06e534fbb57e9d
Ilya Goldyrev <goldirev12@gmail.com>
2018-06-12 13:48:25 +0500

    develop Мобильная верстка страницы Контакты

--- dist/contacts.php
+++ dist/contacts.php
@@ -9,14 +9,16 @@ include($_SERVER["DOCUMENT_ROOT"] . "/blocks/navigation.html"); ?>
   <p class="text"><strong>Офис:</strong> г.Пермь, ул. Дзержинского 15, офис 10</p>
   <p class="text"><strong>Производственная база:</strong> г.Пермь, ул. Дзержинского 47 Литера Б1</p>
   <h2 class="title title-h2">Связаться с нами</h2>
-  <div class="clearfix">
+  <div class="contacts__inner clearfix">
     <a class="link contacts__link link--color-hover" href="tel:+73422371597">Телефон/факс +7 (342) 237 15 97</a>
     <a class="link contacts__link link--color-hover" href="mailto:info@gaross59.ru">Электронная почта:
       info@gaross59.ru</a>
   </div>
   <h2 class="title title-h2">Наше местоположение</h2>
-  <script type="text/javascript" charset="utf-8" async
-          src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Afdac42df5209134c889acf92bd590f2a3abff6b62f9a27c25964c1482b995b23&amp;width=100%25&amp;height=450&amp;lang=ru_RU&amp;scroll=true"></script>
+  <div class="contacts__inner">
+    <script type="text/javascript" charset="utf-8" async
+            src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Afdac42df5209134c889acf92bd590f2a3abff6b62f9a27c25964c1482b995b23&amp;width=100%25&amp;height=450&amp;lang=ru_RU&amp;scroll=true"></script>
+  </div>
   <h2 class="title title-h2">Реквизиты:</h2>
   <p class="text">ИНН 5903099470 КПП 590301001</p>
   <p class="text">р/с: 40702810949490059143 в Волго-Вятском банке ПАО Сбербанк г. Нижний Новгород; кор.счет:
