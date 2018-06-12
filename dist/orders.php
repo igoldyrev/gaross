75ea4548b2628519deb84d3214a552796077cfa4
Ilya Goldyrev <goldirev12@gmail.com>
2018-06-12 15:08:36 +0500

    develop Верстка страницы с заказчиками и наполнение правильным содержимым

--- dist/orders.php
+++ dist/orders.php
@@ -15,39 +15,11 @@ include($_SERVER["DOCUMENT_ROOT"] . "/blocks/navigation.html"); ?>
       </div>
     </div>
     <div class="order">
-      <img class="order__logo" src="/img/orders/ppz-logo.png" alt="Пермский пороховой завод">
+      <img class="order__logo" src="/img/orders/nikiret.png"
+           alt="НАУЧНО-ИССЛЕДОВАТЕЛЬСКИЙ И КОНСТРУКТОРСКИЙ ИНСТИТУТ РАДИОЭЛЕКТРОННОЙ ТЕХНИКИ">
       <div class="order__description">
-        <p class="order__name">ФКП "Пермский пороховой завод"</p>
-        <p class="order__annotation">Одно из крупнейших российских предприятий оборонного комплекса.</p>
-      </div>
-    </div>
-    <div class="order">
-      <img class="order__logo" src="/img/orders/protonpm-logo.png" alt="Протон ПМ">
-      <div class="order__description">
-        <p class="order__name">ПРОТОН ПМ</p>
-        <p class="order__annotation">Публичное акционерное общество «Протон — Пермские моторы» является одним из ведущих
-          предприятий российской космической отрасли.</p>
-      </div>
-    </div>
-    <div class="order">
-      <img class="order__logo" src="/img/orders/goznak-logo.png" alt="ГОЗНАК">
-      <div class="order__description">
-        <p class="order__name">ГОЗНАК</p>
-        <p class="order__annotation">Одно из самых современных полиграфических предприятий в Европе.</p>
-      </div>
-    </div>
-    <div class="order">
-      <img class="order__logo" src="/img/orders/cbkkama.png" alt="ЦБК Кама">
-      <div class="order__description">
-        <p class="order__name">ЦБК Кама</p>
-        <p class="order__annotation">Экслюзивный производитель мелованной бумаги в России!</p>
-      </div>
-    </div>
-    <div class="order">
-      <img class="order__logo" src="/img/orders/galo.png" alt="АО ГалоПолимер-Пермь">
-      <div class="order__description">
-        <p class="order__name">АО "ГалоПолимер-Пермь"</p>
-        <p class="order__annotation">Одно из крупнейших химических предприятий России.</p>
+        <p class="order__name">НАУЧНО-ИССЛЕДОВАТЕЛЬСКИЙ И КОНСТРУКТОРСКИЙ ИНСТИТУТ РАДИОЭЛЕКТРОННОЙ ТЕХНИКИ</p>
+        <p class="order__annotation">Современный научно-производственный комплекс.</p>
       </div>
     </div>
     <div class="order">
@@ -60,28 +32,6 @@ include($_SERVER["DOCUMENT_ROOT"] . "/blocks/navigation.html"); ?>
       </div>
     </div>
     <div class="order">
-      <img class="order__logo" src="/img/orders/iskra.gif" alt="ПАО НПО Искра">
-      <div class="order__description">
-        <p class="order__name">ПАО "НПО Искра"</p>
-        <p class="order__annotation">ПАО «Искра» работает для оборонной, нефтегазовой, машиностроительной,
-          сельскохозяйственной и других отраслей промышленности.</p>
-      </div>
-    </div>
-    <div class="order">
-      <img class="order__logo" src="/img/orders/krym.png" alt="ГУ МЧС России по республике Крым">
-      <div class="order__description">
-        <p class="order__name">ГУ МЧС России по республике Крым</p>
-        <p class="order__annotation"></p>
-      </div>
-    </div>
-    <div class="order">
-      <img class="order__logo" src="/img/orders/lukoil-logo.svg" alt="ЛУКОЙЛ">
-      <div class="order__description">
-        <p class="order__name">ЛУКОЙЛ</p>
-        <p class="order__annotation">Одно из крупнейших предприятий сегмента «геологоразведка, добыча нефти и газа».</p>
-      </div>
-    </div>
-    <div class="order">
       <img class="order__logo" src="/img/orders/metafrax-logo.png" alt="МЕТАФРАКС">
       <div class="order__description">
         <p class="order__name">МЕТАФРАКС</p>
@@ -90,37 +40,56 @@ include($_SERVER["DOCUMENT_ROOT"] . "/blocks/navigation.html"); ?>
       </div>
     </div>
     <div class="order">
-      <img class="order__logo" src="/img/orders/niipm.png" alt="АО НИИПМ">
+      <img class="order__logo" src="/img/orders/apelsin.png" alt="ТЦ Апельсин">
       <div class="order__description">
-        <p class="order__name">АО "НИИПМ"</p>
-        <p class="order__annotation">Одно из крупнейших предприятий оборонно-промышленного комплекса России,
-          занимающихся разработкой перспективных образцов порохов, твёрдых ракетных топлив, малогазовых и
-          воспламеняющихся составов.</p>
+        <p class="order__name">ТЦ Апельсин</p>
+        <p class="order__annotation">Уголок детства в Закамске.</p>
       </div>
     </div>
     <div class="order">
-      <img class="order__logo" src="/img/orders/nikiret.png"
-           alt="НАУЧНО-ИССЛЕДОВАТЕЛЬСКИЙ И КОНСТРУКТОРСКИЙ ИНСТИТУТ РАДИОЭЛЕКТРОННОЙ ТЕХНИКИ">
+      <img class="order__logo" src="/img/orders/kontur.gif" alt="ООО Контур">
       <div class="order__description">
-        <p class="order__name">НАУЧНО-ИССЛЕДОВАТЕЛЬСКИЙ И КОНСТРУКТОРСКИЙ ИНСТИТУТ РАДИОЭЛЕКТРОННОЙ ТЕХНИКИ</p>
-        <p class="order__annotation">Современный научно-производственный комплекс.</p>
+        <p class="order__name">ООО "Контур"</p>
+        <p class="order__annotation">Специализированная организация по экспертизе промышленной
+          безопасности, монтажу, ремонту, реконструкции технических
+          устройств на опасных производственных объектах.</p>
       </div>
     </div>
     <div class="order">
-      <img class="order__logo" src="/img/orders/ros.jpg" alt="ОАО Ростелеком">
+      <img class="order__logo" src="/img/orders/almed.png" alt="Санаторий-профилакторий «Алмед">
       <div class="order__description">
-        <p class="order__name">ОАО "Ростелеком"</p>
-        <p class="order__annotation">Одна из крупнейших в России и Европе телекоммуникационных компаний национального
-          масштаба, присутствующая во всех сегментах рынка услуг связи и охватывающая миллионы домохозяйств в
-          России.</p>
+        <p class="order__name">Санаторий-профилакторий "Алмед"</p>
+        <p class="order__annotation">Лучшее медицинское учреждение Российской Федерации.</p>
       </div>
     </div>
     <div class="order">
-      <img class="order__logo" src="/img/orders/ural.png" alt="ОАО МРСК Урала">
+      <img class="order__logo" src="/img/orders/cbkkama.png" alt="ЦБК Кама">
       <div class="order__description">
-        <p class="order__name">ОАО "МРСК Урала"</p>
-        <p class="order__annotation">Компания, осуществляющая транспорт электроэнергии и присоединение потребителей к
-          электросетям на территории Свердловской, Челябинской областей, а также Пермского края.</p>
+        <p class="order__name">ЦБК Кама</p>
+        <p class="order__annotation">Экслюзивный производитель мелованной бумаги в России!</p>
+      </div>
+    </div>
+    <div class="order">
+      <img class="order__logo" src="/img/orders/rosrezerv.png" alt="ФГКУ Комбинат Алый Стяг Росрезерва">
+      <div class="order__description">
+        <p class="order__name">ФГКУ Комбинат "Алый Стяг" Росрезерва</p>
+        <p class="order__annotation">Федеральное государственное казенное учреждение Комбинат "Алый Стяг" управления
+          федерального агенства по государственным резервам по Приволжскому федеральному округу.</p>
+      </div>
+    </div>
+    <div class="order">
+      <img class="order__logo" src="/img/orders/infguard.gif" alt="НТЦ Информационная безопасность">
+      <div class="order__description">
+        <p class="order__name">НТЦ "Информационная безопасность"</p>
+        <p class="order__annotation">НТЦ «Информационная безопасность» осуществляет весь спектр услуг от обследования,
+          проектирования и создания обьектов защиты информации до их сопровождения и модернизации.</p>
+      </div>
+    </div>
+    <div class="order">
+      <img class="order__logo" src="/img/orders/moto.png" alt="Мотовилихинские заводы">
+      <div class="order__description">
+        <p class="order__name">Мотовилихинские заводы</p>
+        <p class="order__annotation">«Мотовилихинские заводы» — бренд, известный далеко за пределами Пермского края.</p>
       </div>
     </div>
   </div>
