41b4eb5aafdc85980e3bacd9afab21dd5244f7df
Ilia.Goldyrev <Ilia.Goldyrev@eaeconsult.ru>
2019-03-19 19:12:33 +0500

    develop К компоненту Header добавлен адаптив

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 import Logo from './logo.svg';
+import { device } from './../variables';
 
 const HeaderDiv = styled.header`
-width: 100%;
+  width: 100%;
 `;
 
 const HeaderWrap = styled.div`
@@ -12,18 +13,38 @@ const HeaderWrap = styled.div`
   width: 1220px;
   margin: 0 auto;
   padding: 30px 0;
+
+  @media ${device.pad} {
+    width: 100%;
+  }
+
+  @media ${device.mobile} {
+    flex-direction: column;
+    width: 100%;
+    margin-bottom: 15px;
+    padding: 15px 10px;
+    box-sizing: border-box;
+  }
 `;
 
 const HeaderSvg = styled.div`
   width: 420px;
   align-self: center;
+
+  @media ${device.pad} {
+    margin-left: 15px;
+  }
+
+  @media ${device.mobile} {
+    width: 100%;
+  }
 `;
 
 class Header extends React.Component {
   render() {
     return (
       <HeaderDiv>
-        <HeaderWrap>S
+        <HeaderWrap>
           <HeaderSvg>
             <a href="/">
               <Logo />
