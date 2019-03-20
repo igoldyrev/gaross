2df1b56a2c94813b75389fa5af4646fda27740dd
Ilia.Goldyrev <Ilia.Goldyrev@eaeconsult.ru>
2019-03-20 12:40:52 +0500

    develop index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ import React from 'react';
 import { render } from 'react-dom';
 import './components/Body';
 import IndexText from './components/IndexText';
+import Header from './components/header/Header';
+import Footer from './components/footer/footer';
 
-const IndexTextElement = <IndexText text="ggdg" />;
+// const IndexTextElement = <IndexText text="ggdg" />;
+const HeaderWrapElement = <Header />;
+const FooterWrapElement = <Footer />;
 
 render(
-  IndexTextElement,
+  <div>
+    {HeaderWrapElement}
+    {FooterWrapElement}
+    {/* {IndexTextElement} */}
+  </div>,
   document.getElementById('root')
 );
