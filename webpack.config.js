a4cad9b90733afcf93a693a94226e7b88f5bb0f5
Ilia.Goldyrev <Ilia.Goldyrev@eaeconsult.ru>
2019-03-22 11:06:28 +0500

    develop Настройка конфигурации sass и минимизации на production

--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,92 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 //const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: [
+    './src/index.js',
+    './src/style.scss'
+  ],
   output:
   {
       path: path.resolve(__dirname, 'dist'),
-      filename: 'main.js'
+      filename: 'gaross.js'
   },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader"
-          }
-        },
-        {
-          test: /\.svg$/,
-          use: [
-            {
-              loader: "babel-loader"
-            },
-            {
-              loader: "react-svg-loader",
-              options: {
-                jsx: true
-              }
-            }
-          ]
-        }
-      ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        // new CopyWebpackPlugin([
-        //   {
-        //     from: './src/svg',
-        //      to: './svg'
-        //   },
-        //   {
-        //     from: './src/pdf',
-        //     to: './pdf'
-        //   },
-        //   {
-        //     from: './src/pages',
-        //     to: './'
-        //   },
-        //   {
-        //     from: './src/html',
-        //     to: './blocks'
-        //   },
-        //   {
-        //     from: './src/fa',
-        //     to: './fa'
-        //   },
-        //   {
-        //     from: './src/technical_files',
-        //     to: './'
-        //   }
-        // ]),
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true
+      }),
+      new OptimizeCSSAssetsPlugin({})
     ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: "babel-loader"
+          },
+          {
+            loader: "react-svg-loader",
+            options: {
+              jsx: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+            "css-loader",
+            "sass-loader",
+        ]
+      }
+    ]
+  },
+  plugins: [
+      new CleanWebpackPlugin(),
+      new MiniCssExtractPlugin({
+          filename: "gaross.css"
+      })
+      // new CopyWebpackPlugin([
+      //   {
+      //     from: './src/svg',
+      //      to: './svg'
+      //   },
+      //   {
+      //     from: './src/pdf',
+      //     to: './pdf'
+      //   },
+      //   {
+      //     from: './src/pages',
+      //     to: './'
+      //   },
+      //   {
+      //     from: './src/html',
+      //     to: './blocks'
+      //   },
+      //   {
+      //     from: './src/fa',
+      //     to: './fa'
+      //   },
+      //   {
+      //     from: './src/technical_files',
+      //     to: './'
+      //   }
+      // ]),
+  ]
 };
