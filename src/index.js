2d1e2975302c1a8b4893cea571ca667377bc2bbc
Ilia.Goldyrev <Ilia.Goldyrev@eaeconsult.ru>
2019-03-25 18:44:04 +0500

    develop Добавлен react-router

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './components/Body';
 import HeaderWrap from './components/headerWrap/HeaderWrap';
 import Footer from './components/footer/footer';
 
-const HeaderWrapElement = <HeaderWrap />;
-const FooterWrapElement = <Footer />;
-
 render(
-  <div>
-    {HeaderWrapElement}
-    {FooterWrapElement}
-  </div>,
+    <Router>
+      <Route path="/" component={Footer} />
+    </Router>
+  // <div>
+  //   {HeaderWrapElement}
+  //   {FooterWrapElement}
+  // </div>
+  ,
   document.getElementById('root')
 );
