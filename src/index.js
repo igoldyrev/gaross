a592c6761338e8cd6f3e2eeb47d2a4000ad94ff3
Ilya Goldyrev <Ilia.Goldyrev@eaeconsult.ru>
2019-03-28 18:36:30 +0500

    develop Основной файл приложения с роутером

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './components/Body';
 import HeaderWrap from './components/headerWrap/HeaderWrap';
 import Footer from './components/footer/footer';
+import Main from "./components/Main/Main";
 
 render(
-    <Router>
-      <Route path="/" component={Footer} />
-    </Router>
-  // <div>
-  //   {HeaderWrapElement}
-  //   {FooterWrapElement}
-  // </div>
-  ,
+  <Router>
+    <Route component={HeaderWrap} />
+    <Route path={'/'} component={Main} />
+    <Route component={Footer}/>
+  </Router>,
   document.getElementById('root')
 );
