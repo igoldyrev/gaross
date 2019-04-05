6bce2fc5057dedf973f15819e6c782559a280c50
Ilya Goldyrev <Ilia.Goldyrev@eaeconsult.ru>
2019-04-05 13:20:45 +0500

    develop Полный компонент главной страницы

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Wrapper from '../common_components/wrapper/wrapper';
 import IndexTitle from '../index/title/IndexTitle';
 import Text from '../common_components/text/text';
@@ -13,6 +14,18 @@ class Main extends React.Component {
         <Text text="Основное направление деятельности предприятия - комплексные решения в области строительства, проектирования, энергетики, обеспечения безопасности и связи государственных и коммерческих структур, предприятий энергетического, нефтегазового и оборонного комплекса, а также объектов железнодорожного и воздушного транспорта." />
         <H2Title text="Основные виды работ:" />
         <IndexWorkWrap />
+        <H2Title text="Для решения данных задач ООО «ГАРОСС» имеет все необходимые лицензии" />
+        <Text firstText="Свидетельство " secondText=" С – 171-59-0961-59-220317 от 22.03.2017 г.">
+          <Link to="/licenses" className="link">СРО «Строители Урала»</Link>
+        </Text>
+        <Text firstText="Свидетельство " secondText=" № П – 112-59-0170-59-290317 от 29.03.2017 г.">
+          <Link to="/licenses" className="link">СРО «Проектные организации Урала»</Link>
+        </Text>
+        <Text firstText="Лицензия " secondText=" от 13.07.2017 г.">
+          <Link to="/licenses" className="link">МЧС № 59-Б/00513</Link>
+        </Text>
+        <Text firstText="Сертификат соответствия ISO" />
+        <Text firstText="А также располагает высококвалифицированным коллективом проектировщиков, менеджеров, инженеров, монтажников, прошедших обучение и сертификацию." />
       </Wrapper>
     );
   }
