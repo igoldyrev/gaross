33182d471056d1fd415a59fde0cd05580646c440
Ilya Goldyrev <Ilia.Goldyrev@eaeconsult.ru>
2019-04-05 16:05:23 +0500

    develop рефакторинг в функциональный компонент

--- src/components/Licenses/Licenses.js
+++ src/components/Licenses/Licenses.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Wrapper from '../common_components/wrapper/wrapper';
 
-class Licenses extends React.Component {
-  render() {
-    return (
-      <Wrapper />
-    );
-  }
+function Licenses() {
+  return (
+    <Wrapper>
+
+    </Wrapper>
+  );
 }
 
 export default Licenses;
