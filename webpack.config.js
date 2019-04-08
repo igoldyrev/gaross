814eefa46b8bd19fe920d8631b1c428d042e3a11
Ilya Goldyrev <Ilia.Goldyrev@eaeconsult.ru>
2019-04-08 16:14:57 +0500

    develop рефакторинг конфига и добавление копирования pdf

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,29 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: [
     './src/index.js',
-    './src/style.scss'
+    './src/style.scss',
   ],
   output:
   {
     path: path.resolve(__dirname, 'dist'),
     filename: './build/gaross.js',
-    publicPath: '/'
+    publicPath: '/',
   },
   optimization: {
     minimizer: [
       new UglifyJsPlugin({
         cache: true,
-        parallel: true
+        parallel: true,
       }),
-      new OptimizeCSSAssetsPlugin({})
-    ]
+      new OptimizeCSSAssetsPlugin({}),
+    ],
   },
   module: {
     rules: [
@@ -31,67 +31,51 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: 'babel-loader',
+        },
       },
       {
         test: /\.svg$/,
         use: [
           {
-            loader: "babel-loader"
+            loader: 'babel-loader',
           },
           {
-            loader: "react-svg-loader",
+            loader: 'react-svg-loader',
             options: {
-              jsx: true
-            }
-          }
-        ]
+              jsx: true,
+            },
+          },
+        ],
       },
       {
         test: /\.scss$/,
         use: [
           MiniCssExtractPlugin.loader,
-            "css-loader",
-            "sass-loader",
-        ]
-      }
-    ]
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
   },
   plugins: [
-      new CleanWebpackPlugin(),
-      new MiniCssExtractPlugin({
-          filename: "./build/gaross.css"
-      }),
-      new CopyWebpackPlugin([
-        {
-          from: './src/components/index/img',
-           to: './img/index'
-        },
-        {
-          from: './public',
-          to: './'
-        },
-        // {
-        //   from: './src/pdf',
-        //   to: './pdf'
-        // },
-        // {
-        //   from: './src/pages',
-        //   to: './'
-        // },
-        // {
-        //   from: './src/html',
-        //   to: './blocks'
-        // },
-        // {
-        //   from: './src/fa',
-        //   to: './fa'
-        // },
-        // {
-        //   from: './src/technical_files',
-        //   to: './'
-        // }
-      ]),
-  ]
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: './build/gaross.css',
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: './src/components/index/img',
+        to: './img/index',
+      },
+      {
+        from: './public',
+        to: './',
+      },
+      {
+        from: './pdf',
+        to: './pdf',
+      },
+    ]),
+  ],
 };
