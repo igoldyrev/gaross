fb3a61d79252d17a5b6d26b3d45ad6dafaa26c56
Ilya Goldyrev <Ilia.Goldyrev@eaeconsult.ru>
2019-04-11 09:45:53 +0500

    develop стили body перенесены в файл common.scss

--- src/components/Body.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('body').style.background = '#edeef0';
-document.querySelector('body').style.fontSize = '18px';
-document.querySelector('body').style.fontFamily = 'Roboto,Tahoma,sans-serif';
-document.querySelector('body').style.fontWeight = '400';
-document.querySelector('body').style.fontStyle = 'normal';
-document.querySelector('body').style.lineHeight = '24px';
-document.querySelector('body').style.color = '#666';
-document.querySelector('body').style.margin = 0;
-document.querySelector('body').style.padding = 0;
